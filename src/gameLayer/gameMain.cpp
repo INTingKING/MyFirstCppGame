8c71d2870896f150dcc7e7c2541f8b4b226d4955
INTingKING <xenboy18@gmail.com>
2026-03-16 06:26:03 +0100

    added AssetManager

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,20 +1,16 @@
 #include <raylib.h>
 #include "gameMain.h"
 #include <asserts.h>
+#include "assetManager.h"
 
 struct GameData{
-
-    float positionX = 300;
-    float positionY = 200;
-
 }gameData;
+
+AssetManager assetManager;
 
 bool initGame(){
 
-    bool thingsAreOk = false;
-
-    permaAssertComment(thingsAreOk == true, "error; things are not ok :/");
-
+    assetManager.loadAll();
 
     return true;
 }
@@ -24,12 +20,9 @@ bool updateGame(){
     float deltaTime = GetFrameTime();
     if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
 
-    if (IsKeyDown(KEY_A)) { gameData.positionX -= 200 * deltaTime; }
-    if (IsKeyDown(KEY_D)) { gameData.positionX += 200 * deltaTime; }
-    if (IsKeyDown(KEY_W)) { gameData.positionY -= 200 * deltaTime; }
-    if (IsKeyDown(KEY_S)) { gameData.positionY += 200 * deltaTime; }
-
-    DrawRectangle( gameData.positionX, gameData.positionY, 50, 50, {255, 0, 200, 255});
+    DrawTexturePro(assetManager.dirt, 
+        {0, 0, (float)assetManager.dirt.width, (float)assetManager.dirt.height},
+        {50, 50, 100, 100}, {}, 0, WHITE);
 
     return true;
 }
